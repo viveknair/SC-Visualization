b4bad3b23523e4e82e852c6865535242c06f2b73
Kevin Ho <keho98@gmail.com>
2012-12-03 22:30:42 -0800

    Adjusting paths to use area as opposed to line

--- js/script.js
+++ js/script.js
@@ -13,18 +13,29 @@ var marginLeft = 100;
 var marginTop = 30;
 
 
-var x = d3.scale.linear().domain([-10, 10]).range([0, containerWidth]),
+var x = d3.scale.linear().domain([-20, 20]).range([0, containerWidth]),
     y = d3.scale.linear().domain([startingYear,startingYear + 10]).range([0, containerHeight]);
+
+
+var liberalArea = d3.svg.area()
+                          .x(function(d){ return y(d.year)})
+                          .y1(x(0))
+                          .y0(function(d){ return x(d.numCases)})
+                          .interpolate('basis')
+
+var conservativeArea = d3.svg.area()
+                          .x(function(d){ return y(d.year)})
+                          .y1(x(0))
+                          .y0(function(d){ return x(-d.numCases)})
+                          .interpolate('basis')
+
+var liberalData = [{numCases: 10, year: 1970}, {numCases:8, year: 1971}, {numCases:5, year: 1972}, {numCases: 3, year: 1973},{numCases: 7, year: 1974}, {numCases: 4, year: 1975},{numCases:2, year: 1976}, {numCases:9, year: 1977}, {numCases: 3, year: 1978},{numCases: 5, year: 1979}, {numCases: 2, year: 1980}];
+var conservativeData = [{numCases: 4, year: 1970}, {numCases:4, year: 1971}, {numCases:9, year: 1972}, {numCases: 3, year: 1973},{numCases: 9, year: 1974}, {numCases: 4, year: 1975},{numCases:2, year: 1976}, {numCases:8, year: 1977}, {numCases: 8, year: 1978},{numCases: 5, year: 1979}, {numCases: 10, year: 1980}];
+
 
 data = [[{x: 0, year: 1970}, {x:0, year: 1971}, {x:1, year: 1972}, {x: 5, year: 1973},{x: 7, year: 1974}, {x: -4, year: 1975},{x:-2, year: 1976}, {x:1, year: 1977}, {x: 3, year: 1978},{x: 5, year: 1979}, {x: -2, year: 1980}],
 			[{x: -5, year: 1970}, {x:-5, year: 1971}, {x:-2, year: 1972}, {x: -3, year: 1973},{x: -1, year: 1974}, {x: -1, year: 1975},{x:2, year: 1976}, {x:1, year: 1977}, {x: 2, year: 1978},{x: 5, year: 1979}, {x: 6, year: 1980}],
 			[{x: -4, year: 1975},{x:-6, year: 1976}, {x:-10, year: 1977}, {x: -5, year: 1978},{x: -5, year: 1979}, {x: -6, year: 1980}]];
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.x); })
-    .y(function(d,i) { return y(d.year); })
-    .interpolate("basis")
-    .tension(.75);
 
 var xAxis = d3.svg.axis().scale(x).orient("top").ticks(10),
 		yAxis = d3.svg.axis().scale(y).orient("left").ticks(10).tickFormat(d3.format(".0f"));
@@ -40,6 +51,7 @@ pathsContainer = svg.append('g')
 	.attr('transform', 'translate(' + marginLeft + ',0)')
 	.attr("class", "paths_container gradient");
 
+//Add axes and grid lines 
 pathsContainer.selectAll("line.x")
   .data(x.ticks(10))
   .enter().append("line")
@@ -70,11 +82,24 @@ pathsContainer.append("svg:g")
 	.attr("transform", "translate(0," + marginTop + ")")
 	.call(yAxis);
 
-pathsContainer.selectAll("path.justice_path").data(data).enter().append("svg:path")
-	.attr("d", function(d,i){ return line(d); })
-	.attr("class", "justice_path")
-	.attr("transform", "translate(0," + marginTop + ")")
-  	.on("mouseover", function(){ d3.select(this).attr('stroke', 'red')});
+
+// Add actual data
+
+pathsContainer.append('path').datum(liberalData)
+                .attr('class', 'cases liberal')
+                .attr('d', function(d){ return liberalArea(d)})
+                .attr('transform', 'rotate(90) translate(' + [marginTop, -containerWidth] +')')
+
+pathsContainer.append('path').datum(conservativeData)
+                .attr('class', 'cases conservative')
+                .attr('d', function(d){ return conservativeArea(d)})
+                .attr('transform', 'rotate(90) translate(' + [marginTop, -containerWidth] +')')
+
+// pathsContainer.selectAll("path.justice_path").data(data).enter().append("svg:path")
+// 	.attr("d", function(d,i){ return line(d); })
+// 	.attr("class", "justice_path")
+// 	.attr("transform", "translate(0," + marginTop + ")")
+//   	.on("mouseover", function(){ d3.select(this).attr('stroke', 'red')});
 
 
 
