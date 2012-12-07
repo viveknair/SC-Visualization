2494da638d5bf26a40ea7343af824a691c3703de
Forrest Browning <fbrowning@stanford.edu>
2012-12-07 02:43:03 -0800

    Basic Framework For Adjacency Matrix

--- js/adj_matrix.js
+++ js/adj_matrix.js
@@ -1,89 +1,140 @@
-/* Author:
 
-*/
+var margin = {top: 80, right: 0, bottom: 10, left: 80},
+    width = 720,
+    height = 720;
 
-/* Constants */
+var containerWidth = width - 300,
+    containerHeight = height - 300;    
 
-var w = 1000
-var h = 1200;
-var containerWidth = w - 200;
-var containerHeight = h - 200;
-var startingYear = 1970;
-var marginLeft = 100;
-var marginTop = 30;
+var x = d3.scale.ordinal().rangeBands([0, width]),
+    z = d3.scale.linear().domain([0, 4]).clamp(true),
+    c = d3.scale.category10().domain(d3.range(10));
 
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+   .append("g")
+    .attr('width', containerWidth)
+    .attr('height', containerHeight)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scale.linear().domain([-10, 10]).range([0, containerWidth]),
-    y = d3.scale.linear().domain([startingYear,startingYear + 10]).range([0, containerHeight]);
+d3.json("miserables.json", function(miserables) {
+  var matrix = [],
+      nodes = miserables.nodes,
+      n = nodes.length;
 
-data = [[{x: 0, year: 1970}, {x:0, year: 1971}, {x:1, year: 1972}, {x: 5, year: 1973},{x: 7, year: 1974}, {x: -4, year: 1975},{x:-2, year: 1976}, {x:1, year: 1977}, {x: 3, year: 1978},{x: 5, year: 1979}, {x: -2, year: 1980}],
-			[{x: -5, year: 1970}, {x:-5, year: 1971}, {x:-2, year: 1972}, {x: -3, year: 1973},{x: -1, year: 1974}, {x: -1, year: 1975},{x:2, year: 1976}, {x:1, year: 1977}, {x: 2, year: 1978},{x: 5, year: 1979}, {x: 6, year: 1980}],
-			[{x: -4, year: 1975},{x:-6, year: 1976}, {x:-10, year: 1977}, {x: -5, year: 1978},{x: -5, year: 1979}, {x: -6, year: 1980}]];
+  // Compute index per node.
+  nodes.forEach(function(node, i) {
+    node.index = i;
+    node.count = 0;
+    matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
+  });
 
-var line = d3.svg.line()
-    .x(function(d) { return x(d.x); })
-    .y(function(d,i) { return y(d.year); })
-    .interpolate("cardinal")
-    .tension(.75);
+  // Convert links to matrix; count character occurrences.
+  miserables.links.forEach(function(link) {
+    matrix[link.source][link.target].z += link.value;
+    matrix[link.target][link.source].z += link.value;
+    matrix[link.source][link.source].z += link.value;
+    matrix[link.target][link.target].z += link.value;
+    nodes[link.source].count += link.value;
+    nodes[link.target].count += link.value;
+  });
 
-var xAxis = d3.svg.axis().scale(x).orient("top").ticks(10),
-		yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+  // Precompute the orders.
+  var orders = {
+    name: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].name, nodes[b].name); }),
+    count: d3.range(n).sort(function(a, b) { return nodes[b].count - nodes[a].count; }),
+    group: d3.range(n).sort(function(a, b) { return nodes[b].group - nodes[a].group; })
+  };
 
-svg = d3.select("#chart")
-  .append("svg")
-  .attr('width', w)
-  .attr('height', h);
+  // The default sort order.
+  x.domain(orders.name);
 
-pathsContainer = svg.append('g')
-	.attr('width', containerWidth)
-	.attr('height', containerHeight)
-	.attr('transform', 'translate(' + marginLeft + ',0)')
-	.attr("class", "paths_container");
+  svg.append("rect")
+      .attr("class", "background")
+      .attr("width", width)
+      .attr("height", height);
 
-pathsContainer.selectAll("line.x")
-  .data(x.ticks(10))
-  .enter().append("line")
-  .attr("transform", "translate(0," + marginTop + ")")
-  .attr("class", "x")
-  .attr("x1", x)
-  .attr("x2", x)
-  .attr("y1", 0)
-  .attr("y2", containerHeight)
-  .style("stroke", "#ccc");
+  var row = svg.selectAll(".row")
+      .data(matrix)
+    .enter().append("g")
+      .attr("class", "row")
+      .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+      .each(row);
 
-pathsContainer.selectAll("line.y")
-  .data(y.ticks(10))
-  .enter().append("line")
-  .attr("transform", "translate(0," + marginTop + ")")
-  .attr("class", "y")
-  .attr("x1", 0)
-  .attr("x2", containerWidth)
-  .attr("y1", y)
-  .attr("y2", y)
-  .style("stroke", "#ccc");
+  row.append("line")
+      .attr("x2", width);
 
-pathsContainer.append("svg:g")
-	.attr("class", "x axis")
-	.attr("transform", "translate(0," + marginTop + ")")
-	.call(xAxis);
+  row.append("text")
+      .attr("x", -6)
+      .attr("y", x.rangeBand() / 2)
+      .attr("dy", ".32em")
+      .attr("text-anchor", "end")
+      .text(function(d, i) { return nodes[i].name; });
 
-pathsContainer.append("svg:g")
-	.attr("class", "y axis")
-	.attr("transform", "translate(0," + marginTop + ")")
-	.call(yAxis);
+  var column = svg.selectAll(".column")
+      .data(matrix)
+    .enter().append("g")
+      .attr("class", "column")
+      .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
 
-pathsContainer.selectAll("path.justice_path").data(data).enter().append("svg:path")
-	.attr("d", function(d,i){ return line(d); })
-	.attr("class", "justice_path")
-	.attr("transform", "translate(0," + marginTop + ")")
-	.style("stroke-width", 5)
-  	.style("stroke", "steelblue")
-  	.style("fill", "none")
-  	.on("mouseover", function(){ d3.select(this).attr('stroke', 'red')});
+  column.append("line")
+      .attr("x1", -width);
 
+  column.append("text")
+      .attr("x", 6)
+      .attr("y", x.rangeBand() / 2)
+      .attr("dy", ".32em")
+      .attr("text-anchor", "start")
+      .text(function(d, i) { return nodes[i].name; });
 
+  function row(row) {
+    var cell = d3.select(this).selectAll(".cell")
+        .data(row.filter(function(d) { return d.z; }))
+      .enter().append("rect")
+        .attr("class", "cell")
+        .attr("x", function(d) { return x(d.x); })
+        .attr("width", x.rangeBand())
+        .attr("height", x.rangeBand())
+        .style("fill-opacity", function(d) { return z(d.z); })
+        .style("fill", function(d) { return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null; })
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout);
+  }
 
+  function mouseover(p) {
+    d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
+    d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
+  }
 
+  function mouseout() {
+    d3.selectAll("text").classed("active", false);
+  }
 
+  d3.select("#order").on("change", function() {
+    clearTimeout(timeout);
+    order(this.value);
+  });
 
+  function order(value) {
+    x.domain(orders[value]);
 
+    var t = svg.transition().duration(2500);
+
+    t.selectAll(".row")
+        .delay(function(d, i) { return x(i) * 4; })
+        .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+      .selectAll(".cell")
+        .delay(function(d) { return x(d.x) * 4; })
+        .attr("x", function(d) { return x(d.x); });
+
+    t.selectAll(".column")
+        .delay(function(d, i) { return x(i) * 4; })
+        .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+  }
+
+  var timeout = setTimeout(function() {
+    order("group");
+    d3.select("#order").property("selectedIndex", 2).node().focus();
+  }, 5000);
+});
