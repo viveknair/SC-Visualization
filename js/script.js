b4d5ed7605088f8f588fc8449a4cb65cafe508b4
Kevin Ho <keho98@gmail.com>
2012-12-10 21:28:45 -0800

    Fixed merge

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ window.timeline = {
                             .interpolate(interpolation);
 
   var xAxis = d3.svg.axis().scale(x).orient("top").ticks(10).tickFormat(d3.format(".0")),
-  		yAxis = d3.svg.axis().scale(y).orient("left").ticks(numTicks).tickFormat(d3.format(".0f"));
+      yAxis = d3.svg.axis().scale(y).orient("left").ticks(numTicks).tickFormat(d3.format(".0f"));
 
   /* Start rendering svg */
 
@@ -78,10 +78,10 @@ window.timeline = {
               .attr('height', scaleHeight);
 
   pathsContainer = svg.append('g')
-  	.attr('width', containerWidth)
-  	.attr('height', containerHeight)
-  	.attr('transform', 'translate(' + marginLeft + ',0)')
-  	.attr("class", "paths_container gradient");
+    .attr('width', containerWidth)
+    .attr('height', containerHeight)
+    .attr('transform', 'translate(' + marginLeft + ',0)')
+    .attr("class", "paths_container gradient");
 
   //Add axes and grid lines 
   pathsContainer.selectAll("line.x")
@@ -157,14 +157,14 @@ window.timeline = {
     .text("Highlighted Cases");
 
   topScale.append("svg:g")
-  	.attr("class", "x axis")
-  	.attr("transform", "translate(" + [marginLeft, marginTop] + ")")
-  	.call(xAxis);
+    .attr("class", "x axis")
+    .attr("transform", "translate(" + [marginLeft, marginTop] + ")")
+    .call(xAxis);
 
   pathsContainer.append("svg:g")
-  	.attr("class", "y axis")
-  	.attr("transform", "translate(0," + marginTop + ")")
-  	.call(yAxis);
+    .attr("class", "y axis")
+    .attr("transform", "translate(0," + marginTop + ")")
+    .call(yAxis);
 
 
   // Add actual data
@@ -192,104 +192,6 @@ window.timeline = {
       console.log("Clicked data point: " + d);
     });
   }
-}
-
-var maxCases = d3.max([d3.max(liberalData, getCases), d3.max(conservativeData, getCases)]);
-var minYear = d3.min([d3.min(liberalData, getYear), d3.min(conservativeData, getYear)]);
-var maxYear = d3.max([d3.max(liberalData, getYear), d3.max(conservativeData, getYear)]);
-/* Create scales and area layouts */
-var x = d3.scale.linear().domain([-20, 20]).range([0, containerWidth]),
-    y = d3.scale.linear().domain([startingYear,startingYear + 10]).range([0, containerHeight]);
-
-var liberalArea = d3.svg.area()
-                          .x(function(d){ return y(d.year)})
-                          .y1(x(0))
-                          .y0(function(d){ return x(d.numCases)})
-                          .interpolate('basis');
-
-var conservativeArea = d3.svg.area()
-                          .x(function(d){ return y(d.year)})
-                          .y1(x(0))
-                          .y0(function(d){ return x(-d.numCases)})
-                          .interpolate('basis');
-
-var xAxis = d3.svg.axis().scale(x).orient("top").ticks(10).tickFormat(d3.format(".0")),
-		yAxis = d3.svg.axis().scale(y).orient("left").ticks(10).tickFormat(d3.format(".0f"));
-
-/* Start rendering svg */
-
-svg = d3.select("#chart")
-  .append("svg")
-  .attr('width', w)
-  .attr('height', h);
-
-topScale = d3.select("#fixed-scale")
-            .append("svg")
-            .attr('width', scaleWidth)
-            .attr('height', scaleHeight);
-
-pathsContainer = svg.append('g')
-	.attr('width', containerWidth)
-	.attr('height', containerHeight)
-	.attr('transform', 'translate(' + marginLeft + ',0)')
-	.attr("class", "paths_container gradient");
-
-//Add axes and grid lines 
-pathsContainer.selectAll("line.x")
-  .data(x.ticks(numTicks))
-  .enter().append("line")
-  .attr("transform", "translate(0," + marginTop + ")")
-  .attr("class", "x")
-  .attr("x1", x)
-  .attr("x2", x)
-  .attr("y1", 0)
-  .attr("y2", containerHeight);
-
-pathsContainer.selectAll("line.y")
-  .data(y.ticks(numTicks))
-  .enter().append("line")
-  .attr("transform", "translate(0," + marginTop + ")")
-  .attr("class", "y")
-  .attr("x1", 0)
-  .attr("x2", containerWidth)
-  .attr("y1", y)
-  .attr("y2", y);
-
-topScale.append("svg:g")
-	.attr("class", "x axis")
-	.attr("transform", "translate(" + [marginLeft, marginTop] + ")")
-	.call(xAxis);
-
-pathsContainer.append("svg:g")
-	.attr("class", "y axis")
-	.attr("transform", "translate(0," + marginTop + ")")
-	.call(yAxis);
-
-// Add actual data
-pathsContainer.append('path').datum(liberalData)
-                .attr('class', 'cases liberal')
-                .attr('d', function(d){ return liberalArea(d)})
-                .attr('transform', 'rotate(90) translate(' + [marginTop, -containerWidth] +')');
-
-pathsContainer.append('path').datum(conservativeData)
-                .attr('class', 'cases conservative')
-                .attr('d', function(d){ return conservativeArea(d)})
-                .attr('transform', 'rotate(90) translate(' + [marginTop, -containerWidth] +')');
-
-//Adding hoverable decades
-
-pathsContainer.selectAll("rect")
-  .data(y.ticks(numTicks))
-  .enter().append("rect")
-  .attr('class', 'decadeSelector')
-  .attr("transform", "translate(0," + marginTop + ")")
-  .attr("x", 0)
-  .attr("width", containerWidth)
-  .attr("y", function(d,i){ return y(d) - (containerHeight/numTicks)/2})
-  .attr("height", function(d,i){ if(i === 0 || i === numTicks - 1) { return containerHeight/numTicks/2; } else return containerHeight/numTicks;})  
-  .on('click', function(d){
-    console.log("Clicked data point: " + d);
-  });
 }
 
 window.timeline.initialize();
